a6b62e03c820b1cdc29c239c456826133762d26a
juan.reyesl <juan.reyesl@LSTKAG261561.SofttekClients.com>
2021-07-01 03:46:20 -0500

    The  tests were added

---
+++ src/test/java/com/clip/challenge/ControllerTest.java
@@ -0,0 +1,208 @@
+package com.clip.challenge;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+import java.net.URL;
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
+
+import com.clip.challenge.config.SpringConfiguration;
+import com.clip.challenge.dto.TransactionDTO;
+import com.clip.challenge.repository.TransactionRepository;
+import com.clip.challenge.service.TransactionService;
+
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+@ActiveProfiles({ "h2IntegrationTests" })
+@ContextConfiguration(classes = SpringConfiguration.class)
+class ControllerTest {
+	@Autowired
+	TransactionService transactionService;
+
+	@Autowired
+	TransactionRepository transactionRepository;
+
+	@LocalServerPort
+	private int port;
+
+	@Autowired
+	private TestRestTemplate restTemplate;
+
+	@Test
+	void contextLoads() {
+		List<TransactionDTO> findByClipUserOrderByClipUser = transactionService.findByClipUserOrderByClipUser("juan");
+		assertNotNull(findByClipUserOrderByClipUser);
+	}
+
+	// @Test
+	public void addTransactioinTest() {
+		TransactionDTO transactionDTO = new TransactionDTO();
+		transactionDTO.setAmount(300d);
+		transactionDTO.setClipUser("test");
+		transactionDTO.setCarddata("3495834583438204");
+		HttpEntity<TransactionDTO> requestEntity = new HttpEntity<>(transactionDTO);
+		ResponseEntity<TransactionDTO> response = this.restTemplate.exchange(
+				"http://localhost:" + port + "/transaction", HttpMethod.POST, requestEntity, TransactionDTO.class);
+		assertEquals("test", response.getBody().getClipUser());
+		assertEquals("3495834583438204", response.getBody().getCarddata());
+		assertNotNull(response.getBody().getDate());
+		assertNotNull(response.getBody().getId());
+		assertEquals(false, response.getBody().isPaid());
+		assertEquals(200, response.getStatusCodeValue());
+	}
+
+	@Test
+	public void addTransactioinWithOutClipUserParameterTest() {
+		TransactionDTO transactionDTO = new TransactionDTO();
+		transactionDTO.setAmount(300d);
+		transactionDTO.setCarddata("3495834583438204");
+		HttpEntity<TransactionDTO> requestEntity = new HttpEntity<>(transactionDTO);
+		ResponseEntity<String> response = this.restTemplate.exchange("http://localhost:" + port + "/transaction",
+				HttpMethod.POST, requestEntity, String.class);
+		assertEquals(true, response.getBody().contains("clipUser parameter is mandatory"));
+		assertEquals(true, response.getBody().contains("400"));
+		assertEquals(true, response.getBody().contains("BAD_REQUEST"));
+		assertEquals(400, response.getStatusCodeValue());
+	}
+
+	@Test
+	public void addTransactioinWithOutCardDataParameterTest() {
+		TransactionDTO transactionDTO = new TransactionDTO();
+		transactionDTO.setAmount(300d);
+		transactionDTO.setClipUser("test");
+		HttpEntity<TransactionDTO> requestEntity = new HttpEntity<>(transactionDTO);
+		ResponseEntity<String> response = this.restTemplate.exchange("http://localhost:" + port + "/transaction",
+				HttpMethod.POST, requestEntity, String.class);
+		assertEquals(true, response.getBody().contains("carddata parameter is mandatory"));
+		assertEquals(true, response.getBody().contains("400"));
+		assertEquals(true, response.getBody().contains("BAD_REQUEST"));
+	}
+
+	@Test
+	public void addTransactioinWithOutAmountParameterTest() {
+		TransactionDTO transactionDTO = new TransactionDTO();
+		transactionDTO.setClipUser("test");
+		transactionDTO.setCarddata("3495834583438204");
+		HttpEntity<TransactionDTO> requestEntity = new HttpEntity<>(transactionDTO);
+		ResponseEntity<String> response = this.restTemplate.exchange("http://localhost:" + port + "/transaction",
+				HttpMethod.POST, requestEntity, String.class);
+		assertEquals(true, response.getBody().contains("amount parameter is mandatory"));
+		assertEquals(true, response.getBody().contains("400"));
+		assertEquals(true, response.getBody().contains("BAD_REQUEST"));
+	}
+
+	@Test
+	public void addTransactioinWithOutBodyTest() {
+		HttpEntity<String> requestEntity = new HttpEntity<>("data");
+		ResponseEntity<String> response = this.restTemplate.exchange("http://localhost:" + port + "/transaction",
+				HttpMethod.POST, requestEntity, String.class);
+		assertEquals(true, response.getBody().contains("415"));
+		assertEquals(true, response.getBody().contains("Unsupported Media Type"));
+	}
+
+	@Test
+	public void addTransactioWithHttpDiferentTest() {
+		TransactionDTO transactionDTO = new TransactionDTO();
+		transactionDTO.setAmount(300d);
+		transactionDTO.setClipUser("test");
+		transactionDTO.setCarddata("3495834583438204");
+		HttpEntity<TransactionDTO> requestEntity = new HttpEntity<>(transactionDTO);
+		ResponseEntity<String> response = this.restTemplate.exchange("http://localhost:" + port + "/transaction",
+				HttpMethod.DELETE, requestEntity, String.class);
+		assertEquals(true, response.getBody().contains("400"));
+		assertEquals(true, response.getBody().contains("BAD_REQUEST"));
+		assertEquals(true, response.getBody().contains("not supporte"));
+		assertEquals(400, response.getStatusCodeValue());
+	}
+
+	@Test
+	public void getTransactionByClipUserTest() throws Exception {
+		ResponseEntity<String> response = restTemplate
+				.getForEntity(new URL("http://localhost:" + port + "/transaction/test").toString(), String.class);
+		assertEquals(200, response.getStatusCodeValue());
+	}
+
+	@Test
+	public void getTransactionByClipUserTestWithData() throws Exception {
+		ResponseEntity<Object[]> response = restTemplate
+				.getForEntity(new URL("http://localhost:" + port + "/transaction/juan").toString(), Object[].class);
+		assertEquals(true, response.getBody().length >= 0);
+		assertEquals(200, response.getStatusCodeValue());
+	}
+
+	@Test
+	public void getTransactionByClipUserFailPathTest() throws Exception {
+		ResponseEntity<String> response = restTemplate
+				.getForEntity(new URL("http://localhost:" + port + "/transactionn/juan").toString(), String.class);
+		assertEquals(404, response.getStatusCodeValue());
+		assertEquals(true, response.getBody().contains("Not Found Exception"));
+		assertEquals(true, response.getBody().contains("404"));
+		assertEquals(true, response.getBody().contains("NOT_FOUND"));
+	}
+
+	@Test
+	public void findByClipUserOrderByClipUserTest() throws Exception {
+		ResponseEntity<String> response = restTemplate
+				.getForEntity(new URL("http://localhost:" + port + "/disbursement").toString(), String.class);
+		assertEquals(200, response.getStatusCodeValue());
+	}
+
+	@Test
+	public void findByClipUserOrderByClipUserTestWithData() throws Exception {
+		ResponseEntity<Object[]> response = restTemplate
+				.getForEntity(new URL("http://localhost:" + port + "/disbursement").toString(), Object[].class);
+		assertEquals(true, response.getBody().length > 0);
+		assertEquals(200, response.getStatusCodeValue());
+	}
+
+	@Test
+	public void findByClipUserOrderByClipUserFailPathTest() throws Exception {
+		ResponseEntity<String> response = restTemplate
+				.getForEntity(new URL("http://localhost:" + port + "/disbursement2").toString(), String.class);
+		assertEquals(404, response.getStatusCodeValue());
+		assertEquals(true, response.getBody().contains("Not Found Exception"));
+		assertEquals(true, response.getBody().contains("404"));
+		assertEquals(true, response.getBody().contains("NOT_FOUND"));
+	}
+
+	
+	@Test
+	public void makeDisbursementTest() throws Exception {
+		ResponseEntity<Object[]> response = restTemplate
+				.postForEntity(new URL("http://localhost:" + port + "/disbursement").toString(), null, Object[].class);
+		assertEquals(true, response.getBody().length >= 0);
+		assertEquals(200, response.getStatusCodeValue());
+	}
+
+	@Test
+	public void makeDisbursementFailPathTest() throws Exception {
+		ResponseEntity<String> response = restTemplate
+				.postForEntity(new URL("http://localhost:" + port + "/disbursementt").toString(), null, String.class);
+		assertEquals(404, response.getStatusCodeValue());
+		assertEquals(true, response.getBody().contains("Not Found Exception"));
+		assertEquals(true, response.getBody().contains("404"));
+		assertEquals(true, response.getBody().contains("NOT_FOUND"));
+	}
+
+	@Test
+	public void makeDisbursementNotSupportHttpTest() throws Exception {
+		ResponseEntity<String> response = this.restTemplate.exchange("http://localhost:" + port + "/transaction",
+				HttpMethod.DELETE, null, String.class);
+		assertEquals(true, response.getBody().contains("400"));
+		assertEquals(true, response.getBody().contains("BAD_REQUEST"));
+		assertEquals(true, response.getBody().contains("not supporte"));
+		assertEquals(400, response.getStatusCodeValue());
+	}
+
+}
