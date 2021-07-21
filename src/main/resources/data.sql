1bc65ee40a26bee75c9d59975c12c2d01d85c76b
juan.reyesl <juan.reyesl@LSTKAG261561.SofttekClients.com>
2021-07-20 22:15:20 -0500

    SQL sentences was modified

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,9 +1,9 @@
 
-insert into transaction (amount, card_data, clip_user, date, id_disbursement, paid) 
-values (30, '3720945348398463','juan', '2020-02-15', 0, false),
-		(30, '3720945348398463','juan', '2020-02-15', 0, false),
-		(50, '3720945348398463','juan', '2020-02-15', 0, false),
-		(35, '3720945348398463','juan', '2020-02-15', 0, false),
-		(100, '3720945348398463','juan', '2020-02-15', 0, false),
-		(2000, '3720945348398463','juan', '2020-02-15', 0, false),
-		(500, '3720945348398463','juan', '2020-02-15', 0, false);
+insert into transactions (amount, card_data, clip_user, date, id_disbursement, paid) 
+values (30, '3720945348398463','juan', '2020-02-15', null, false),
+		(30, '3720945348398463','juan', '2020-02-15', null, false),
+		(50, '3720945348398463','juan', '2020-02-15', null, false),
+		(35, '3720945348398463','juan', '2020-02-15', null, false),
+		(100, '3720945348398463','juan', '2020-02-15', null, false),
+		(2000, '3720945348398463','juan', '2020-02-15', null, false),
+		(500, '3720945348398463','juan', '2020-02-15', null, false);
