1752303284413f98bb1ef10fc80ab1ab5f8782fe
juan.reyesl <juan.reyesl@LSTKAG261561.SofttekClients.com>
2021-07-20 22:37:41 -0500

    business logic was added

--- src/main/java/com/clip/challenge/service/DisbursementService.java
+++ src/main/java/com/clip/challenge/service/DisbursementService.java
@@ -1,83 +1,13 @@
 package com.clip.challenge.service;
 
-import java.sql.Timestamp;
-import java.text.SimpleDateFormat;
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
-import java.util.concurrent.atomic.AtomicInteger;
-import java.util.stream.Collectors;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 import com.clip.challenge.dto.DisbursementDTO;
-import com.clip.challenge.dto.TransactionDTO;
-import com.clip.challenge.repository.DisbursementRepository;
-import com.clip.challenge.repository.TransactionRepository;
 
-@Service
-public class DisbursementService {
-	@Autowired
-	TransactionRepository transactionRepository;
-	
-	@Autowired	
-	DisbursementRepository disbursementRepository;
+public interface DisbursementService {
 
-	@Transactional
-	public List<DisbursementDTO> makeDisbursement() {		
-		List<DisbursementDTO> disbursements= new ArrayList<DisbursementDTO>();
-		Map<DisbursementDTO,List<TransactionDTO>> disbursementAndTransaction = new HashMap<>();
-		List<TransactionDTO> transactions = transactionRepository.findTransactionNoPaid();		
-		
-		Map<String, Double> totalAumountByUser  = transactions.stream().collect(
-				Collectors.groupingBy(TransactionDTO::getClipUser, 
-			    Collectors.summingDouble(TransactionDTO::getAmount)));
+	public List<DisbursementDTO> makeDisbursement();
 
-		totalAumountByUser.forEach((k, v) -> {
-			List<TransactionDTO> filterByUser = transactions.stream().
-					filter(
-							t -> t.getClipUser().equals(k))
-					.collect(Collectors.toList());
-
-			DisbursementDTO disbursement = new DisbursementDTO(k, v);			
-			disbursementAndTransaction.put(disbursement, filterByUser);
-			disbursementRepository.save(disbursement);
-			disbursements.add(disbursement);
-			transactionRepository.updatePaidAndId(Boolean.TRUE, getListIds(filterByUser),disbursement.getId());			
-		});		
-		return disbursements;
-	}
-	
-	public List<Integer> getListIds(List<TransactionDTO> transactions) {
-		List<Integer> ids = 
-				transactions.stream()
-			              .map( TransactionDTO::getId)
-			              .collect(Collectors.toList());
-		return ids;		
-	}
-
-	public List<String> getAllDisbursement() {
-		List<DisbursementDTO> findAllDisbursementDTOOrderByClipUserAndId = disbursementRepository.findAllDisbursementDTOOrderByClipUserAndId();
-		List<String> disbursements =new  ArrayList<>();
-		 AtomicInteger counter = new AtomicInteger(0);
-		findAllDisbursementDTOOrderByClipUserAndId.forEach(d ->{
-			disbursements.add( new String ("disbursement "+ counter.incrementAndGet()+": " + d.getTotalamount()+" pesos - "+d.getClipUser()+" - date: "+ formatDateToDdMmYy(d.getDate())   ));
-		});			
-	    return disbursements;
-	}
-	
-	public String formatDateToDdMmYy(Timestamp timestamp) {
-		String pattern = "dd/MM/yy";
-		SimpleDateFormat simpleDateFormat = new SimpleDateFormat(pattern);
-		String dateString = simpleDateFormat.format(timestamp);
-		return dateString;
-		
-	}
-	
-	
-	
+	public List<String> getAllDisbursement();
 
 }
--- src/main/java/com/clip/challenge/service/TransactionService.java
+++ src/main/java/com/clip/challenge/service/TransactionService.java
@@ -2,24 +2,11 @@ package com.clip.challenge.service;
 
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
 import com.clip.challenge.dto.TransactionDTO;
-import com.clip.challenge.repository.TransactionRepository;
 
-@Service
-public class TransactionService {
-	@Autowired
-	TransactionRepository transactionRepository;
-
-	public List<TransactionDTO> findByClipUserOrderByClipUser(String clipUser) {
-		return transactionRepository.findByClipUserOrderByClipUser(clipUser);
-	}
-
-	public TransactionDTO save(TransactionDTO transaction) {
-		transactionRepository.save(transaction);
-		return transaction;
-	}
+public interface  TransactionService {
+	public List<TransactionDTO> findByClipUserOrderByClipUser(String clipUser);
+	
+	public TransactionDTO save(TransactionDTO transaction);
 
 }
